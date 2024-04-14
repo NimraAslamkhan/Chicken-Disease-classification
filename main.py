8f5fa9b62c790a6417b6c0a68581d6295ae28626
“nimraaslam3132@gmail.com <nimraaslam3132@gmail.com>
2024-04-14 11:16:16 +0500

    first commit

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-import logger
-logger.info("welcom to my custom log")
+import logging
+logging.info('This is an informational message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
